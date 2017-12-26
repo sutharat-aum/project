0752d869ff2f81619b3bbb73b0413ff2823d8143
sutharat-aum <sutharat.aum@gmail.com>
2017-12-26 11:16:31 +0700

    2017/12/26

--- classify3.py
+++ classify3.py
@@ -1,12 +1,14 @@
 import re
 from textblob.classifiers import NaiveBayesClassifier
-import itertools
+import pickle
+import unicodedata as uni
 
-# FILE_NAME = ['happiness/text_emotion_happiness70','sadness/text_emotion_sadness70','love/text_emotion_love70','hate/text_emotion_hate70','fun/text_emotion_fun70','worry/text_emotion_worry70']
-FILE_NAME = ['happiness/text_emotion_happiness70','sadness/text_emotion_sadness70']
+FILE_NAME = ['happiness/text_emotion_happiness70','sadness/text_emotion_sadness70','love/text_emotion_love70','hate/text_emotion_hate70','fun/text_emotion_fun70','worry/text_emotion_worry70']
+# FILE_NAME_neg = 'worry/text_emotion_worry70'
 emotion = ['happiness','sadness','love','hate','fun','worry']
-# FILE_NAME_test = ['happiness/text_emotion_happiness30','sadness/text_emotion_sadness30','love/text_emotion_love30','hate/text_emotion_hate30','fun/text_emotion_fun30','worry/text_emotion_worry30']
-FILE_NAME_test = ['happiness/text_emotion_happiness30','sadness/text_emotion_sadness30']
+FILE_NAME_test = ['happiness/text_emotion_happiness30','sadness/text_emotion_sadness30','love/text_emotion_love30','hate/text_emotion_hate30','fun/text_emotion_fun30','worry/text_emotion_worry30']
+# FILE_NAME_neg_test  = 'worry/text_emotion_worry30'
+
 def processTweet(tweet):
     tweet = tweet.lower()
     tweet = re.sub('((www\.[^\s]+)|(https?://[^\s]+))', 'URL', tweet)
@@ -17,11 +19,10 @@ def processTweet(tweet):
 
 def process(FILE_NAME,emotion):
     train = []
-    train2 = []
-    for i in range(0,len(FILE_NAME)):
+    for i in range(0,1):
         with open(FILE_NAME[i], 'r') as fh:
-            for line_pos in fh:
-                line_pos = fh.readline()
+            all = fh.readlines()
+            for line_pos in all:
                 processedTweet = processTweet(line_pos)
                 word_list_pos = processedTweet.replace(',', '') \
                     .replace('@', '') \
@@ -46,7 +47,6 @@ def process(FILE_NAME,emotion):
                     .replace('AT_USER', '')
                 sentence = (word_list_pos, emotion[i])
                 train.append(sentence)
-
         fh.close()
     return train
 
@@ -54,31 +54,50 @@ train = process(FILE_NAME,emotion)
 test = process(FILE_NAME_test,emotion)
 cl = NaiveBayesClassifier(train)
 
+f = open('train.pkl', 'wb')
+pickle.dump(cl, f)
+f.close()
 
-# for i,j in enumerate(test):
-#     print(i)
-#     print(j)
+f = open('train.pkl', 'rb')
+cl = pickle.load(f)
+f.close()
+
+# numbers_list = cl.train()
+# list_pickle_path = 'train.pkl'
 #
-# for i in range(len(test)):
-#     num = test.index(test[i])
-#     print(len(test))
+# # Create an variable to pickle and open it in write mode
+# list_pickle = open(list_pickle_path, 'wb')
+# pickle.dump(numbers_list, list_pickle)
+# list_pickle.close()
 
-acc = 0
 
-for i,temp in range(len(test)):
-    a = cl.classify(temp[0])
-        # print("acc :" + temp[1] + " result :" + a)
-        # print(a==temp[1])
-    if temp[1]==a:
-        acc = acc+1
-    else:
-            # print(str(tt)+str(i))
-            #print(str(i)+temp[0])
-        print(len(test)+" ------- "+"acc :" + temp[1] +" sen : "+ temp[0] +" result :" + a)
+# f = open('train.pckl', 'rb')
+# cl = pickle.load(f)
+# f.close()
+#dumfile
+# print(len(test))
 
+
+# acc = 0
+# for i in range(0,len(test)):
+#     a = cl.classify(test[i][0])
+#     if test[i][1] == a:
+#         acc = acc+1
+#     else:
+#         print("line number "+ str(i) +" acc :" + test[i][1] +" sen : "+ test[i][0] +" result :" + a)
+
+# for temp in test:
+#     a = cl.classify(temp[0])
+#     # print("acc :" + temp[1] + " result :" + a)
+#     # print(a==temp[1])
+#     if temp[1]==a:
+#         acc = acc+1
+#     # else:
+#     #     print(temp[0])
+#     #     print("acc :" + temp[1] +" sen : "+ temp[0] +" result :" + a)
 
 # print(acc/test.__len__())
-#
+
 # print("Accuracy: {0}".format(cl.accuracy(test)))
 
 # cl.show_informative_features(5)
